ee7534aa26ce5ca81a5cea85323a225ddb8ca7af
koyih <rhumbotz5726@gmail.com>
2026-01-20 15:03:20 -0600

    added limlight stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,13 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Constants.OIConstants;
 import frc.robot.commands.Apriltagcmd;
 import frc.robot.subsystems.Drivesubsystem;
 
@@ -50,10 +52,37 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-    new JoystickButton(driver, 1).whileTrue( new Apriltagcmd());
+       new JoystickButton(driver, 2).whileTrue(new RunCommand(() -> driveSub.drive(
+      -MathUtil.applyDeadband(driver.getLeftY()/slow, OIConstants.kDriveDeadband),
+      -MathUtil.applyDeadband(driver.getLeftX()/slow, OIConstants.kDriveDeadband),
+         -(LimelightHelpers.getTX("")+0) * 0.015, false), driveSub));
+
+    new JoystickButton(driver, 3).whileTrue(new RunCommand(() -> driveSub.drive(
+     getTY(-2,-0.05),
+      -MathUtil.applyDeadband(driver.getLeftX()/slow, OIConstants.kDriveDeadband), 
+      getTX(0, 0.015), 
+        false
+      )));
   }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
+  }
+
+  
+  public double getTY(double offset, double scale){
+    if(LimelightHelpers.getFiducialID("") == -1){
+      return LimelightHelpers.getTY("");
+    } else {
+      return (LimelightHelpers.getTY("")+offset) * scale;
+    }
+  }
+
+  public double getTX(double offset, double scale){
+    if(LimelightHelpers.getFiducialID("") == -1){
+      return LimelightHelpers.getTX("");
+    } else {
+      return  -(LimelightHelpers.getTX("")+offset) * scale ;
+    }
   }
 }
